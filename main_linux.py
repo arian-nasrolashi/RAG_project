1abaabd911864ae25143156c54e6353320065f15
arian-nasrollashi <aghaarian823@gmail.com>
2026-08-17 14:22:25 +0330

    Add Linux API code

---
+++ main_linux.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import requests
+
+app = FastAPI()
+
+
+class LLMRequest(BaseModel):
+    question: str
+    context: str
+
+
+@app.get("/")
+def home():
+    return {"message": "LLM API is working"}
+
+
+@app.post("/generate")
+def generate(data: LLMRequest):
+
+    prompt = f"""
+بر اساس اطلاعات زیر به سؤال کاربر پاسخ بده.
+فقط از اطلاعات موجود در Context استفاده کن.
+
+Context:
+{data.context}
+
+Question:
+{data.question}
+"""
+
+    response = requests.post(
+        "http://127.0.0.1:11434/api/generate",
+        json={
+            "model": "qwen2.5:3b",
+            "prompt": prompt,
+            "stream": False
+        },
+        timeout=120
+    )
+
+    response.raise_for_status()
+
+    result = response.json()
+
+    return {
+        "answer": result["response"]
+    }
